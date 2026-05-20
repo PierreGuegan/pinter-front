4f97a5af979bab7f52c769f94f06cc4c9e5f9ddf
pierre guegan <pierre.guegan@e-artsup.net>
2026-05-20 19:07:47 +0200

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,9 +15,11 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/profile/profile').then(m => m.ProfileComponent)
   },
 
-  // 🔥 AJOUT ICI
   {
     path: 'login',
     loadComponent: () => import('./pages/login/login').then(m => m.LoginComponent)
-  }
+  },
+  
+  { path: 'register',
+     loadComponent: () => import('./pages/register/register').then(m => m.RegisterComponent) },
 ];
