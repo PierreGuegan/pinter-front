927318dd1e5d50e95b6ad4fc0f4819f9d9ef72c5
pierre guegan <pierre.guegan@e-artsup.net>
2026-05-21 15:28:12 +0200

    CSS page Post

--- src/app/pages/post/post.ts
+++ src/app/pages/post/post.ts
@@ -5,20 +5,75 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
+  styleUrl: './post.css',
   imports: [FormsModule, CommonModule],
   selector: 'app-post',
-  template: `
-    <h2>Upload image</h2>
+  template: `,
+  
+<div class="post-page">
 
-    <input type="file" (change)="onFileSelected($event)" />
+  <div class="post-card">
 
-    <input [(ngModel)]="title" placeholder="title" />
-    <input [(ngModel)]="description" placeholder="description" />
+    <h2>Create post</h2>
 
-    <button (click)="upload()">Post</button>
+    <!-- IMAGE PREVIEW -->
+    <div class="preview-container" *ngIf="previewUrl">
+      <img [src]="previewUrl" class="preview-image" />
+    </div>
 
-    <p *ngIf="message">{{ message }}</p>
-  `
+    <!-- UPLOAD BUTTON -->
+    <label class="upload-box">
+
+      <input
+        type="file"
+        accept="image/*"
+        (change)="onFileSelected($event)"
+        hidden
+      />
+
+      <div class="upload-content">
+
+        <svg viewBox="0 0 24 24" class="upload-icon">
+          <path
+            d="M12 16V4M7 9l5-5 5 5M5 20h14"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+          />
+        </svg>
+
+        <p>Select an image</p>
+
+      </div>
+
+    </label>
+
+    <!-- INPUTS -->
+    <input
+      [(ngModel)]="title"
+      placeholder="Title"
+      class="input"
+    />
+
+    <textarea
+      [(ngModel)]="description"
+      placeholder="Description"
+      class="textarea"
+    ></textarea>
+
+    <!-- BUTTON -->
+    <button class="post-button" (click)="upload()">
+      Publish
+    </button>
+
+    <p class="message" *ngIf="message">
+      {{ message }}
+    </p>
+
+  </div>
+
+</div>
+`
 })
 export class PostComponent {
 
@@ -29,9 +84,22 @@ export class PostComponent {
 
   constructor(private imageService: ImageService) {}
 
+  previewUrl: string | ArrayBuffer | null = null;
+
   onFileSelected(event: any) {
-    this.file = event.target.files[0];
-  }
+
+  this.file = event.target.files[0];
+
+  if (!this.file) return;
+
+  const reader = new FileReader();
+
+  reader.onload = () => {
+    this.previewUrl = reader.result;
+  };
+
+  reader.readAsDataURL(this.file);
+}
 
   upload() {
     if (!this.file) {
