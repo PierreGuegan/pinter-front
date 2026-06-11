9d98f1270917bf9c46f55b44bc9d86d5e6c3d86e
pierre guegan <pierre.guegan@e-artsup.net>
2026-06-11 12:28:19 +0200

    double clic pour like ne persiste pas

--- src/app/components/image-modal/image-modal.ts
+++ src/app/components/image-modal/image-modal.ts
@@ -2,6 +2,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImageService } from '../../services/imagesService';
 import { FormsModule } from '@angular/forms';
+import { OnChanges, SimpleChanges } from '@angular/core';
 
 
 @Component({
@@ -11,7 +12,7 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./image-modal.css'],
   imports: [CommonModule, FormsModule]
 })
-export class ImageModalComponent {
+export class ImageModalComponent implements OnChanges{
 
   @Input() image: any;
   @Output() close = new EventEmitter<void>();
@@ -29,12 +30,6 @@ export class ImageModalComponent {
   comments: any[] = [];
   newComment: string = '';
   isLiked: boolean = false;
-
-
-ngOnInit() {
-  this.reload();
-    console.log("MODAL CREATED");
-}
 
   constructor(private imageService: ImageService) {}
 
@@ -55,15 +50,28 @@ loadLikeState() {
 }
 
 toggleLike() {
+  const previous = this.isLiked;
+
+  // optimistic UI
+  this.isLiked = !previous;
+  this.likesCount += previous ? -1 : 1;
+
   this.imageService.toggleLike(this.image.id).subscribe({
     next: () => {
       this.loadLikes();
       this.loadLikeState();
+    },
+    error: () => {
+      // rollback si erreur
+      this.isLiked = previous;
+      this.loadLikes();
     }
   });
 }
 
 loadComments() {
+  if (!this.image?.id) return;
+
   this.imageService.getComments(this.image.id).subscribe({
     next: (data) => this.comments = data
   });
@@ -88,5 +96,16 @@ reload() {
   this.loadLikeState();
 }
 
+ngOnChanges(changes: SimpleChanges) {
+  if (changes['image'] && this.image?.id) {
+    this.resetState();   // 👈 ajout
+    this.reload();
+  }
+}
 
+resetState() {
+  this.likesCount = 0;
+  this.comments = [];
+  this.isLiked = false;
+}
 }
